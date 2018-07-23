ed58b00963665cc8a16153596028d6ebdc87e533
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-07-23 15:10:00 -0400

    Remove auto tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_07_23_174246) do
-
+ActiveRecord::Schema.define(version: 2018_07_23_175843) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
