5aa5f1e09a3c76a90a94ba568421c363eb969a6b
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-07-23 11:30:56 -0400

    Add user signup page and controller function

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,11 +3,36 @@ class UserController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new
+
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save?
+      redirect_to user_path(@user)
+
+    else
+      render :new
+
+    end
+
   end
 
   def show
+    load_user
+    
   end
+
+
+  private
+
+  def load_user
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'user/new'
-  get 'user/create'
-  get 'user/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user
 end
