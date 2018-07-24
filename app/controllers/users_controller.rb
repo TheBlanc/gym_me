a57719c5b166999757c8a0d3690d2e9d320ead92
Israelistic <haguyle@gmail.com>
2018-07-24 12:02:21 -0400

    linked all pages register, login, logout. added condition for user not being able to add event unless logged

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,14 @@
 class UsersController < ApplicationController
 
-
-
   def index
   end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new
-
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
-
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      render :new
-    end
-
-  end
-
   def show
     load_user
   end
-
-  def update
-    load_user
-      if @user.update_attributes(user_param)
-        redirect_to users_path
-      else
-        render :edit
-  end
-
-  def edit
-      load_user
-  end
-
-def destory
-  load_user
-  @user.destroy
-  redirect_to events_path
-end
-
   private
 
   def load_user
-    @user = User.find(params[:id])
+    @user = current_user
   end
-  def user_param
-    params.require(:user).permit(first_name:, :last_name, :email, phone_number: , :password, :password_confirmation)
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "events#index"
-  resources :events
+    root to: "events#index"
+    devise_for :users
+    resources :users, only: [:show]
+    resources :events
 end
