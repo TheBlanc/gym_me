66fe95181e7902c4da8842fc6df5f4b5b052f83e
Israelistic <haguyle@gmail.com>
2018-07-24 18:10:26 -0400

    ==CHANGE LOG==: 1) changed DB from Sqlight to postgress to accommodate search. 2)added gem textacular. 3)edited  index method in the Events controller to diplay searched.4)created form_tag in the application view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
- 
+
   def show
-    load_user
+      @user = current_user
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_24_031017) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "location"
