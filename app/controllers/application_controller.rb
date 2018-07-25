69cfd27dfbe4cb9a870a26b22d40c7e6153191a3
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-07-25 17:24:06 -0400

    Unlock devise controllers. Change route of profile update to go to profile show page. Remove address column from Users. Remove location column from users. Change sex to be string in users. Add address columns to Events.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-# allow the passing of the strong params (first_name, sex, etc) in devise
- # along with other params into account_update
+  # allow the passing of the strong params (first_name, sex, etc) in devise
+  # along with other params into account_update
   def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :location, :fitness_level, :sex, :description, address: [:street_address, :city, :state, :zip]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :location, :fitness_level, :sex, :description, :street_address, :city, :province, :postal_code, :country])
   end
+
+  # Overwrite the devise path after updating a profile
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
+
 
 
   helper_method :joined_event?
