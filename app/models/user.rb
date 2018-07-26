211ea0d4ff283f04dca9b47cbfe7f08e5c2e8976
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-07-26 17:29:04 -0400

    addset up ActionCable and code a mini-chat app with support for multiple rooms. The app includes both the client and server sides

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,17 @@ class User < ApplicationRecord
   has_many :tickets
   has_many :events, through: :tickets
   has_many :owned_events, foreign_key: "user_id", class_name: "Event"
+# dependent destory allows you to call User.destroy and delete all chat rooms own by user
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :chat_rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
+
+  def name
+    email.split('@')[0]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
+    mount ActionCable.server => '/cable'
     root to: "events#index"
     devise_for :users, controllers: { registrations: 'users/registrations' }
     resources :users, only: [:show]
+    resources :chat_rooms, only: [:new, :create, :show, :index]
     resources :events do
       resources :tickets, only: [:create]
     end
