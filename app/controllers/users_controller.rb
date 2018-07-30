7b2e7a13ef125d976aef200aa27c7218667747b7
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-07-30 15:37:59 -0400

    1) Create migrations to add matching:boolean location:string longatude and latitude float, and change sex to gender in users table. 2) Create user index page with basic search function.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,32 @@
 class UsersController < ApplicationController
 
+  def index
+    # check if search parameter is being passed and isnt an empty string
+    if params[:search] && !params[:search].empty?
+
+      search_users = User.basic_search(params[:search])
+      # iterate through the users and check if the they are looking for gym buddy (ie. user.matching == true)
+      available_users = []
+      search_users.each do |user|
+        if user.matching == true
+          available_users << user
+        end
+      end
+      @users = available_users
+
+    else
+      all_users = User.all
+      available_users = []
+
+      all_users.each do |user|
+        if user.matching == true
+          available_users << user
+        end
+      end
+      @users = available_users
+    end
+
+  end
 
   def show
       @user = User.find(params[:id])
@@ -17,13 +44,13 @@ class UsersController < ApplicationController
     @user.first_name = params[:user][:first_name]
     @user.last_name = params[:user][:last_name]
     @user.age = params[:user][:age]
-    @user.sex = params[:user][:sex]
-    @user.country = params[:user][:country]
-    @user.province = params[:user][:province]
-    @user.city = params[:user][:city]
-    @user.street_address = params[:user][:street_address]
-    @user.postal_code = params[:user][:postal_code]
+    @user.gender = params[:user][:gender]
+    @user.fitness_level = params[:user][:fitness_level]
+    @user.location = params[:user][:location]
+    @user.description = params[:user][:description]
+    @user.matching = params[:user][:matching]
     @user.avatar.attach(params[:user][:avatar])
+
     if @user.save
       redirect_to user_path(@user.id)
     else
