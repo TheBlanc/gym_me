58dbc07cb9eb1b85b80957f14f4ff856ece6acc6
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-08-01 17:23:15 -0400

    Add front-end logic for user menu display

--- app/assets/javascripts/profile_nav.js
+++ app/assets/javascripts/profile_nav.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  var avatarButton = document.querySelector('#profile-avatar-button')
-  var userMenu = document.querySelector('#user-menu')
+  var avatarButton = document.querySelector('div#profile-avatar-button');
+  var userMenu = document.querySelector('#user-menu');
 
   avatarButton.addEventListener("click", function(e) {
     $('#user-menu').toggleClass('hidden');
+  });
+
+  // Add event listener to the document to hide the user menu when a user clicks outside the menu and avatar area
+  document.addEventListener("click", function(e) {
+    if ( e.target.closest('#user-menu') != userMenu && e.target.closest('div#profile-avatar-button') != avatarButton ) {
+      $('#user-menu').addClass('hidden');
+      }
   });
 
 
