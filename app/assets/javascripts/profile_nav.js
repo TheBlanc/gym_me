84192f84afe01ca4fda6c2ec11092caa101826a4
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-08-02 00:21:17 -0400

    1) Add svg icons 2) Add incomplete styling for user menu and home page.

--- app/assets/javascripts/profile_nav.js
+++ app/assets/javascripts/profile_nav.js
@@ -2,17 +2,28 @@ document.addEventListener("DOMContentLoaded", function(){
 
   var avatarButton = document.querySelector('div#profile-avatar-button');
   var userMenu = document.querySelector('#user-menu');
+  var closeButton = document.querySelector('#close-button');
 
   avatarButton.addEventListener("click", function(e) {
     $('#user-menu').toggleClass('hidden');
   });
 
-  // Add event listener to the document to hide the user menu when a user clicks outside the menu and avatar area
-  document.addEventListener("click", function(e) {
-    if ( e.target.closest('#user-menu') != userMenu && e.target.closest('div#profile-avatar-button') != avatarButton ) {
-      $('#user-menu').addClass('hidden');
-      }
+  closeButton.addEventListener("click", function(e) {
+    $('#user-menu').toggleClass('hidden');
   });
+
+
+  // For desktop
+  // Add event listener to the document to hide the user menu when a user clicks outside the menu and avatar area
+  // document.addEventListener("click", function(e) {
+  //   if ( e.target.closest('#user-menu') != userMenu && e.target.closest('div#profile-avatar-button') != avatarButton ) {
+  //     $('#user-menu').addClass('hidden');
+  //     }
+  // });
+
+
+
+
 
 
 });
