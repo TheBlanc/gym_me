c6b64808e9585e8f4c620e21d6f27892224dcda5
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-08-02 23:43:34 -0400

    Add icon and style to home page. Fix user modal style.

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,6 @@ end
   e.address = "100 King St. W"
   e.description = "This is event #{n+1}"
   e.capacity = 2
-  e.activity_type = "Cardio"
   e.user_id = (n+1)
   e.time = Time.now + 1000000
   e.persistence = false
